edf59d10b82bf9c05b494b552ed5c6dca9c3cb7d
Parag <paragdurafe@outlook.com>
2021-09-26 23:35:44 +0530

    📱 Handles

--- components/social.js
+++ components/social.js
@@ -14,7 +14,7 @@ function Social() {
                 <SocialLinks image="linkedin" text="linkedIn" link="https://www.linkedin.com/in/parag-durafe-b991b1191/" />
                 <SocialLinks image="stackoverflow" text="Stackoverflow" link="https://stackoverflow.com/users/16798712/parag-d" />
                 <SocialLinks image="twitter" text="Twitter" link="https://twitter.com/ParagDurafe" />
-                <SocialLinks image="gmail" text="mail" link="paragdurafe25@gmail.com" />
+                <SocialLinks image="gmail" text="mail" link="mailto:paragdurafe25@gmail.com" />
             </div>
         </Section>
     )
