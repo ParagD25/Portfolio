5239a5a54e75479180950e97c9b651faa02fe1dd
Parag <paragdurafe@outlook.com>
2021-11-03 22:47:36 +0530

    💡 Feat: Credit Section

--- components/footer.js
+++ components/footer.js
@@ -4,10 +4,10 @@ function Footer() {
     return (
         <footer className={classes.footer}>
             <div className={classes.developed}>
-                Developed By <a href="https://github.com/ParagD25/Portfolio" target="_blank" rel="noopener noreferrer" className={classes.link}>Parag Durafe</a>
+                Developed By <a href="https://github.com/ParagD25/Portfolio" target="_blank" rel="noopener noreferrer" className={classes.link}>Parag Durafe</a> &amp; <a href="https://github.com/bilal-23/personal-portfolio" target="_blank" rel="noopener noreferrer" className={classes.link}>Bilal Mansuri</a>
             </div>
             <div className={classes.design}>
-                Design by <a href="https://github.com/bilal-23/personal-portfolio" target="_blank" rel="noopener noreferrer" className={classes.link}>Bilal Mansuri</a>, <a href="https://github.com/bchiang7/v3" target="_blank" rel="noopener noreferrer" className={classes.link}>Brittany</a> &amp; <a href="https://github.com/adeolaadeoti/adeolaadeoti-portfolio" target="_blank" rel="noopener noreferrer" className={classes.link}>Adeolaadeoti</a>
+                Design by <a href="https://github.com/bchiang7/v3" target="_blank" rel="noopener noreferrer" className={classes.link}>Brittany</a> &amp; <a href="https://github.com/adeolaadeoti/adeolaadeoti-portfolio" target="_blank" rel="noopener noreferrer" className={classes.link}>Adeolaadeoti</a>
             </div>
         </footer>
     )
